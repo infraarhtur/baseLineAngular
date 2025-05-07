cef9d71c106dbe09f2566a7de1ed691ae4567624
infraarhtur <infraarhtur@gmail.com>
2025-05-07 12:29:16 -0500

    -lm: primeros cambios de productos

--- src/app/features/products/components/form-products/form-products.component.ts
+++ src/app/features/products/components/form-products/form-products.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-products',
@@ -6,6 +7,37 @@ import { Component } from '@angular/core';
   templateUrl: './form-products.component.html',
   styleUrl: './form-products.component.scss'
 })
-export class FormProductsComponent {
+export class FormProductsComponent implements OnInit {
+
+  @Input() productData?: any;
+  @Output() formSubmitted = new EventEmitter<any>();
+
+  productForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+
+  buildForm(): void {
+    this.productForm = this.fb.group({
+      name: [this.productData?.name || '', [Validators.required, Validators.maxLength(100)]],
+      description: [this.productData?.description || '', [Validators.maxLength(255)]],
+      sale_price: [this.productData?.sale_price || 0, [Validators.required, Validators.min(0)]],
+      purchase_price: [this.productData?.purchase_price || 0, [Validators.required, Validators.min(0)]],
+      stock: [this.productData?.stock || 0, [Validators.required, Validators.min(0)]],
+      provider_id: [this.productData?.provider_id || '', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.productForm.valid) {
+      this.formSubmitted.emit(this.productForm.value);
+    } else {
+      this.productForm.markAllAsTouched();
+    }
+  }
 
 }
