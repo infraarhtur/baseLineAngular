d61fc0ad79c09f3efb62ee8142155600e9353c8b
infraarhtur <infraarhtur@gmail.com>
2025-05-07 16:10:27 -0500

    -lm: se inicia con el update de productos

--- src/app/features/products/components/create-products/create-products.component.ts
+++ src/app/features/products/components/create-products/create-products.component.ts
@@ -13,7 +13,7 @@ import { SnackbarService } from '../../../../shared/services/snackbar.service';
 })
 export class CreateProductsComponent {
 
-  @Input() clientData?: any;
+  @Input() productData?: any;
   @Output() formSubmitted = new EventEmitter<any>();
 
 
@@ -25,8 +25,7 @@ export class CreateProductsComponent {
   ) {}
 
   createProduct(formData: any): void {
-    debugger;
-    this.productsService.createProduct(formData).subscribe({
+      this.productsService.createProduct(formData).subscribe({
       next: () => {
         this.snackbar.success('✅ producto creado con éxito')
         this.router.navigate(['/products/select']);
--- src/app/features/products/components/update-products/update-products.component.ts
+++ src/app/features/products/components/update-products/update-products.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute,Router } from '@angular/router';
+import { ProductsService } from '../../services/products.service';
+import { SnackbarService } from '../../../../shared/services/snackbar.service';
 
 @Component({
   selector: 'app-update-products',
@@ -6,6 +10,52 @@ import { Component } from '@angular/core';
   templateUrl: './update-products.component.html',
   styleUrl: './update-products.component.scss'
 })
-export class UpdateProductsComponent {
+export class UpdateProductsComponent implements OnInit {
+  productId!: string; // ✅ Aquí se almacena el ID recibido
+  @Input() productData?: any;
+  @Output() formSubmitted = new EventEmitter<any>();
+
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private snackbar: SnackbarService,
+    private productsService: ProductsService, // Inyectamos el servicio
+    private router: Router // Para redirigir después de guardar
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.productId = String(params.get('id')); // ✅ Obtener el ID de la URL
+      console.log('producto a actualizar:', this.productId);
+      this.loadProduct();
+    });
+  }
+
+  loadProduct():void{
+    this.productsService.getProductById(this.productId).subscribe({
+      next: (data) => {
+        this.productData = data;
+        console.log('producto adaptado para el formulario:', this.productData);
+      },
+      error: (err) => {
+        console.error('Error al cargar el producto:', err);
+        this.snackbar.error('❌ No se pudo cargar el producto');
+      }
+    });
+  }
+
+  updateProduct(data: any): void {
+    this.productsService.updateProduct(this.productId, data).subscribe({
+    next: () => {
+      this.snackbar.success('✅ producto creado con éxito')
+      this.router.navigate(['/products/select']);
+    },
+    error: (error) => {
+      console.error('Error al crear producto:', error);
+      this.snackbar.error('❌ Ocurrió un error al actualizar el producto.');
+    }
+  });
+}
+
 
 }
