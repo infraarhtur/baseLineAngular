bce524ebe1745dd1fdb0e53bc63f7bb0679530c7
infraarhtur <infraarhtur@gmail.com>
2025-05-16 23:17:12 -0500

    -lm: se hace el crud de providers

--- src/app/features/providers/components/form-providers/form-providers.component.ts
+++ src/app/features/providers/components/form-providers/form-providers.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, OnChanges, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProvidersService } from '../../../providers/services/providers.service';
 
 @Component({
   selector: 'app-form-providers',
@@ -6,6 +8,40 @@ import { Component } from '@angular/core';
   templateUrl: './form-providers.component.html',
   styleUrl: './form-providers.component.scss'
 })
-export class FormProvidersComponent {
+export class FormProvidersComponent implements OnInit, OnChanges {
+
+  @Input() providerData?: any;
+  @Output() formSubmitted = new EventEmitter<any>();
+
+  providerForm!: FormGroup;
+  constructor(private fb: FormBuilder,
+      private providersService: ProvidersService
+    ) {}
+
+    ngOnInit(): void {
+      this.buildForm();
+    }
+    ngOnChanges(changes: SimpleChanges): void {
+      if (changes['providerData'] && this.providerData) {
+        this.buildForm();
+      }
+
+    }
+
+    buildForm(): void {
+      this.providerForm = this.fb.group({
+        name: [this.providerData?.name || '', [Validators.required, Validators.maxLength(100)]],
+        phone: [this.providerData?.phone || '', [Validators.required, Validators.pattern('^\\+?[0-9]{7,15}$')]],
+        email: [this.providerData?.email || '', [Validators.required, Validators.email, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$')]], // Mejor validación,
+        address: [this.providerData?.address || '',[Validators.maxLength(60)]],
+
+      });
+    }
+
+    onSubmit(): void {
+      if (this.providerForm.valid) {
+        this.formSubmitted.emit(this.providerForm.value);
+      }
+    }
 
 }
