21ea49e470ce42de5f40e500809a4cdbd1bc7437
infraarhtur <infraarhtur@gmail.com>
2025-05-28 15:58:08 -0500

    -lm: sales primer select

--- src/app/features/sales/components/select-sales/select-sales.component.ts
+++ src/app/features/sales/components/select-sales/select-sales.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { SalesService } from '../../services/sales.service';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../../../shared/components/confirm-dialog/confirm-dialog.component';
+import { SnackbarService } from '../../../../shared/services/snackbar.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { ViewChild } from '@angular/core'
+
 
 @Component({
   selector: 'app-select-sales',
@@ -6,6 +16,42 @@ import { Component } from '@angular/core';
   templateUrl: './select-sales.component.html',
   styleUrl: './select-sales.component.scss'
 })
-export class SelectSalesComponent {
+export class SelectSalesComponent implements OnInit, AfterViewInit {
+
+  displayedColumns: string[] = [ 'actions','client_id','sale_date', 'total_amount', 'status','payment_method','comment']; // ✅ Columnas de la tabla
+
+  sales: any[] = []; // Lista de ventas
+  selection = new SelectionModel<any>(true, []);
+  dataSource = new MatTableDataSource<any>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator
+
+  constructor(
+    private salesService: SalesService,
+    private router: Router,
+    private dialog: MatDialog,
+    private snackbar: SnackbarService
+  ) {}
+  ngOnInit(): void {
+    this.loadSales();
+  }
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+    loadSales(): void {
+    this.salesService.getSales().subscribe({
+      next: (data) => {
+        this.dataSource.data = data;
+        console.log('Ventass cargadas:', this.dataSource.data );
+        this.snackbar.success('Ventas cargados');
+            },
+      error: (err) => {
+        this.snackbar.error('Error al obtener clientes');
+        console.error('Error al obtener clientes', err);
+      }
+    });
+  }
+
 
 }
--- src/app/features/sales/services/sales.service.ts
+++ src/app/features/sales/services/sales.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SalesService {
+  private apiUrl = `${environment.apiUrl}api/sale`; // URL del backend
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+   /**
+   * Obtiene la lista las ventas desde el backend
+   */
+  getSales(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+  // trae una venta por ID (UUID)
+  getSaletById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+
 }
